62360b6f9bc0390995fe5ac8a5047d8cf0de68d2
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2022-01-04 16:53:26 +0100

    bumped to version 0.2.10

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='binx-og-image-generator',
-    version="0.2.9",
+    version="0.2.10",
     url='https://github.com/binxio/og-image-generator',
     license="restricted",
     author='Mark van Holsteijn',
